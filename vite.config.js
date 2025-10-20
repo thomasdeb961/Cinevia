b1faf92d927325bb4c02d785cf1a14738fcaef21
DEBBO THOMAS <thomas.debbo@my-digital-school.org>
2025-10-20 17:22:34 +0200

    mes modif après import

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,19 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
+        // Pages à la racine
         main: resolve(__dirname, 'index.html'),
-        superman: resolve(__dirname, 'superman.html'),
+        f1: resolve(__dirname, 'f1.html'),
+        gameofthrone: resolve(__dirname, 'gameofthrone.html'),
+        impossible: resolve(__dirname, 'impossible.html'),
+        été: resolve(__dirname, 'lété.html'),
+        lilo: resolve(__dirname, 'lilo.html'),
+        mercredi: resolve(__dirname, 'mercredi.html'),
+
+        // Pages dans le dossier /pages
+        films: resolve(__dirname, 'pages/films.html'),
+        maliste: resolve(__dirname, 'pages/maliste.html'),
+        series: resolve(__dirname, 'pages/series.html'),
       },
     },
   },
